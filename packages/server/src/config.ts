a656717c74e45bb78ea0798095b46786c4f75cda
Lucas Diniz <lucas.mdiniz@live.com>
2020-10-18 09:26:17 -0300

    first commit

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -3,12 +3,10 @@ import dotenvSafe from 'dotenv-safe';
 
 const root = path.join.bind(this, __dirname, '../');
 
-if (process.env.NOW_REGION) {
-  dotenvSafe.load({
-    path: root('.env'),
-    sample: root('.env.example'),
-  });
-}
+dotenvSafe.config({
+  path: root('.env'),
+  sample: root('.env.example'),
+});
 
 const ENV = process.env;
 
